dee51bc8c8cbc39937c4c27eafc9a4bc2f008dfb
tbr <tbr@tbr.com>
2026-09-08 10:47:20 +0200

    biostats

---
+++ biostats/src/fr/epita/biostat/datamodel/BiostatEntry.java
@@ -0,0 +1,51 @@
+package fr.epita.biostat.datamodel;
+
+public class BiostatEntry {
+    private String name;
+    private String gender;
+    private Integer age;
+    private Integer height;
+    private Integer weight;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Integer getHeight() {
+        return height;
+    }
+
+    public void setHeight(Integer height) {
+        this.height = height;
+    }
+
+    public Integer getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Integer weight) {
+        this.weight = weight;
+    }
+
+
+}
---
+++ biostats/src/fr/epita/biostat/launcher/Main.java
@@ -0,0 +1,11 @@
+package fr.epita.biostat.launcher;
+
+import fr.epita.biostat.datamodel.BiostatEntry;
+
+public class Main {
+
+    static void main(String[] args) {
+        BiostatEntry entry = new BiostatEntry();
+
+    }
+}
