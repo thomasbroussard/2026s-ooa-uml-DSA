f55c852414f988a739f7f2cf812debf6a5080a90
tbr <tbr@tbr.com>
2026-09-22 13:43:16 +0200

    tdd

--- biostats/test/fr/epita/biostat/test/services/TestJDBCDAO.java
+++ biostats/test/fr/epita/biostat/test/services/TestJDBCDAO.java
@@ -4,9 +4,12 @@ import fr.epita.biostat.datamodel.BiostatEntry;
 import fr.epita.biostat.service.BiostatJdbcDAO;
 
 import java.sql.SQLException;
+import java.util.List;
 
 public class TestJDBCDAO {
     static void main() throws SQLException {
+
+        //given
         BiostatJdbcDAO dao = new BiostatJdbcDAO();
         System.out.println(dao.findAll());
         dao.save(new BiostatEntry("Thomas", "M", 25, 175, 65));
@@ -14,7 +17,17 @@ public class TestJDBCDAO {
         dao.update(new BiostatEntry("Thomas", "M", 26, 175, 65));
         System.out.println(dao.findAll());
 
+        //when
         dao.delete(new BiostatEntry("Thomas", null, 0, 0, 0));
-        System.out.println(dao.findAll());
+
+        //then
+
+        List<BiostatEntry> list = dao.findAll();
+        if (list.isEmpty()){
+            System.out.println("List is empty: success");
+        }else{
+            System.out.println("List is not empty: failure");
+            System.out.println(list);
+        }
     }
 }
